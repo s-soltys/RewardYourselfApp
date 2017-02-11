739ee757d4cb2b854e99e5443dcfe40a3413523f
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2017-02-11 11:46:17 +0100

    Adjust task card animation.

--- src/app/task/animations/discard-card.animation.ts
+++ src/app/task/animations/discard-card.animation.ts
@@ -2,8 +2,8 @@ import { trigger, state, style, transition, animate } from '@angular/core';
 
 export const DiscardCardAnimation = function(){
     return trigger(`discardCard`, [
-        state(`show`, style({ opacity: 1, transform: `translateX(0)`, filter: 'blur(0)' })),
-        state(`void`, style({ opacity: 0, transform: `translateX(100px)`, filter: 'blur(3px)' })),
+        state(`show`, style({ opacity: 1, transform: `scale(1)`, filter: 'blur(0)' })),
+        state(`void`, style({ opacity: 0, transform: `scale(0.75)`, filter: 'blur(3px)' })),
         transition(`show => void`, animate(`0.5s 0s ease-out`))
     ]);
 }
