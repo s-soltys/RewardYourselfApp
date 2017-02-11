f651518a67a166a7bffca39a9e0de55ba44d9a92
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2017-02-11 01:11:51 +0100

    Fix task form animation issues.

--- src/app/task/animations/fade.animation.ts
+++ src/app/task/animations/fade.animation.ts
@@ -2,9 +2,9 @@ import { trigger, state, style, transition, animate } from '@angular/core';
 
 export const FadeAnimation = function(time: number){
     return trigger(`fade`, [
-        state(`void`, style({ opacity: 0.25, filter: 'blur(4px) contrast(90%)' })),
-        state(`show`, style({ opacity: 1, filter: 'blur(0px) contrast(100%)' })),
-        transition(`void => show`, animate(`${time}s 0s ease-out`)),
-        transition(`show => void`, animate(`${time}s 0s ease-in`))
+        state(`void`, style({ opacity: 0.25, filter: 'blur(4px)' })),
+        state(`show`, style({ opacity: 1, filter: 'blur(0px)' })),
+        transition(`void => show`, animate(`${time}s`)),
+        transition(`show => void`, animate(`${time}s`))
     ]);
 }
--- src/app/task/animations/fly-from-bottom.animation.ts
+++ src/app/task/animations/fly-from-bottom.animation.ts
@@ -2,9 +2,9 @@ import { trigger, state, style, transition, animate } from '@angular/core';
 
 export const FlyInFromBottomAnimation = function(hiddenOffset: number){
     return trigger(`flyInFromBottom`, [
-        state(`void`, style({ transform: `translate(${hiddenOffset}px, ${hiddenOffset}px) scale(1.5)` })),
-        state(`show`, style({ transform: `translate(0, 0) scale(1)` })),
-        transition(`void => show`, animate(`0.5s 0s cubic-bezier(0.075, 0.82, 0.165, 1)`)),
+        state(`void`, style({ transform: `translateY(${hiddenOffset}px) ` })),
+        state(`show`, style({ transform: `translateY(0)` })),
+        transition(`void => show`, animate(`0.5s 0s ease-out`)),
         transition(`show => void`, animate(`0.5s 0s ease-in`))
     ]);
 }
